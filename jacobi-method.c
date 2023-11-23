4e035d488de3e22a5f5986a0623c3a283b7e57e8
Pipluppp <duncanb013@gmail.com>
2023-11-24 01:09:37 +0800

    Clean up, add Diagonal Checker

--- jacobi-method.c
+++ jacobi-method.c
@@ -5,49 +5,60 @@
 #define ERROR_SET 0.01
 
 int main() {
-    // Matrix
-    int a[3][3] = { {3, 1, 1}, 
-                    {2, 6, 3}, 
-                    {2, 1, 4}};
+    // Matrix of coefficients of the system
+    int matrix[3][3] = { {3, 1, 1}, 
+                         {2, 6, 3}, 
+                         {2, 1, 4} };
     
     // Resulting vector
-    int vec[3] = {1, 2, 3};
+    int resulting_vec[3] = {1, 2, 3};
     
-    // Vars
+    /* An extra copy to have access to the current and also the previous iteration's approximated variables
+    For each iteration, tmp_approx holds the new calculated variables, but then we copy it to approx by the end */
     float approx[3] = {0, 0, 0};
-    float tmp_approx[3] = {0, 0, 0};
+    float tmp_approx[3] = {0, 0, 0}; 
     
+    // Error variables
     float error = 1;
     float err[3] = {0};
     
-    int iter = 0;
+    int iter = 0; 
     
-    // TODO: Diagonal checker
-    
+    // Check if Diagonally Dominant
+    for (int i = 0; i < MATRIX_SIZE; i++)
+    {
+        float diag_var = matrix[i][i];
+
+        for (int j = 0; j < MATRIX_SIZE; j++)
+            if (i != j)
+                diag_var -= matrix[i][j];
+        
+        if (diag_var <= 0)
+        {
+            printf("Not Diagonally Dominant\n");
+            break;
+        }
+    }
+
+    // TODO: Check if possible to be rearranged into Diagonally Dominant
+    // TODO: Rearrange to Diagonally Dominant
+
     // Jacobi Method Algorithm
     while (error >= ERROR_SET)
-    {
-        iter++;
-        
+    {   
         for (int i = 0; i < MATRIX_SIZE; i++)
-        {
-            // Add resulting vector coefficient
-            tmp_approx[i] += vec[i];
-            
+        {            
             // Subtract dot product of row with approx (i != j)
             for (int j = 0; j < MATRIX_SIZE; j++)
-            {
-                if (i == j)
-                    continue;
-                    
-                tmp_approx[i] -= a[i][j] * approx[j];
-            }
-            
-            // Divide by coefficient of current
-            tmp_approx[i] /= a[i][i];
-        }
+                if (i != j)
+                    tmp_approx[i] -= matrix[i][j] * approx[j];
+
+            tmp_approx[i] += resulting_vec[i]; // Add resulting vector coefficient
+
+            tmp_approx[i] /= matrix[i][i]; // Divide whole expression by coefficient of var being solved
+        } 
         
-        // Error calculations
+        // Calculate errors
         error = 0;
         for (int i = 0; i < MATRIX_SIZE; i++)
         {
@@ -67,11 +78,8 @@ int main() {
         
         // Reset tmp_approx
         for (int i = 0; i < MATRIX_SIZE; i++)
-        {
             tmp_approx[i] = 0;
-        }
     
+        iter++;
     }
-
-    return 0;
 }
